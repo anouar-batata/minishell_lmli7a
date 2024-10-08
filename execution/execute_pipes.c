e4c282058129fe28e807d7a4b5cced9dcc724240
Anouar Louriga <alouriga@e2r4p7.1337.ma>
2024-10-08 17:39:24 +0100

    redirection

--- execution/execute_pipes.c
+++ execution/execute_pipes.c
@@ -6,7 +6,7 @@
 /*   By: alouriga <alouriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 10:57:41 by alouriga          #+#    #+#             */
-/*   Updated: 2024/10/08 00:17:11 by alouriga         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:32:00 by alouriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,40 @@ void execution_first_command(char **command)
 int   first_execution(char **command, t_commands *cmds, int *fd)
 {
 	t_shell *env =  env_control(GET_ENV, 0, 0);
-	
-	// if(check_built_ins(command, env) == 0)
-	// 	return (0);
+	int bkp_0 = dup(0);
+    int bkp_1 = dup(1);
     int pid = fork();
     if (pid == 0)
     {
         close(fd[0]);
         dup2(fd[1], STDOUT_FILENO);
         close(fd[1]);
-        execution_first_command(command); // **fd = alloc(pipes);
+        if (check_the_redirection(cmds) == -1)
+        {
+            printf("No such file or directory\n");
+            return(-1);
+        }
+        execution_first_command(command);
+        dup2(bkp_1, 1);
+        dup2(bkp_0, 0);
+        close(bkp_0);
+        close(bkp_1);
     }
     else
+    {
+        dup2(bkp_1, 1);
+        dup2(bkp_0, 0);
+        close(bkp_0);
+        close(bkp_1);
         close(fd[1]);
+    }
 	return (1);
 }
-int   middle_execution(char **command, int *fd, int s)
+int   middle_execution(char **command, t_commands *cmds, int *fd, int s)
 {
 	t_shell *env =  env_control(GET_ENV, 0, 0);
-	
+	int bkp_0 = dup(0);
+    int bkp_1 = dup(1);
     pipe(fd);
     int pid = fork();
     if (pid == 0)
@@ -62,20 +77,34 @@ int   middle_execution(char **command, int *fd, int s)
         close(s);
         dup2(fd[1], STDOUT_FILENO);
         close(fd[1]);
+        if (check_the_redirection(cmds) == -1)
+        {
+            printf("No such file or directory\n"); //to do
+            return(-1);
+        }
         execution_first_command(command);
+        dup2(bkp_1, 1);
+        dup2(bkp_0, 0);
+        close(bkp_0);
+        close(bkp_1);
     }
     else
     {
+        dup2(bkp_1, 1);
+        dup2(bkp_0, 0);
+        close(bkp_0);
+        close(bkp_1);
         close(s);
         close(fd[1]);
     }
 	return (1);
 }
 
-int    finale_execution(char **command, int *fd, int s )
+int    finale_execution(char **command, t_commands *cmds, int *fd, int s )
 {
 	t_shell *env =  env_control(GET_ENV, 0, 0);
-	
+	int bkp_0 = dup(0);
+    int bkp_1 = dup(1);
 	// if(check_built_ins(command, env) == 0)
 	// 	return (0);
     int pid = fork();
@@ -84,7 +113,23 @@ int    finale_execution(char **command, int *fd, int s )
         close(fd[1]);
         dup2(s, STDIN_FILENO);
         close(s);
+    if (check_the_redirection(cmds) == -1)
+    {
+        printf("No such file or directory\n");
+        return(-1);
+    }
         execution_first_command(command);
+        dup2(bkp_1, 1);
+        dup2(bkp_0, 0);
+        close(bkp_0);
+        close(bkp_1);
+    }
+    else
+    {
+        dup2(bkp_1, 1);
+        dup2(bkp_0, 0);
+        close(bkp_0);
+        close(bkp_1);
     }
     return (pid);
 }
@@ -108,18 +153,18 @@ void    execute_pipes(t_commands *commands)
         else
         {
             nb_of_nds = ft_lstsize_2(tmp);
-           first_execution(tmp->command, commands ,fd);
+           first_execution(tmp->command, tmp ,fd);
            save_fd = fd[0];
             tmp = tmp->next;
             nb_of_nds -= 1;
             while (i < nb_of_nds - 1)
             {
-                middle_execution(tmp->command, fd, save_fd);
+                middle_execution(tmp->command, tmp, fd, save_fd);
                 save_fd = fd[0];
                 tmp = tmp->next;
                 i++;
             }
-            pid_of_last_command = finale_execution(tmp->command, fd, save_fd);
+            pid_of_last_command = finale_execution(tmp->command, tmp, fd, save_fd);
         }
     close(fd[0]);
     close(fd[1]);
--- execution/redirection.c
+++ execution/redirection.c
@@ -6,51 +6,106 @@
 /*   By: alouriga <alouriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 23:42:01 by alouriga          #+#    #+#             */
-/*   Updated: 2024/10/07 18:53:26 by alouriga         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:14:57 by alouriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int   check_the_redirection(t_commands *command)
+// int   check_the_redirection(t_commands *command)
+// {
+//     t_redir *curr;
+//     int fd;
+//     curr = command->redir_lst;
+//     while (curr)
+//     {
+//         if (curr->redir_type == REDIR_OUT)
+//         {
+//            fd = open(curr->file, O_WRONLY | O_CREAT  | O_TRUNC, 0644);
+//            if (!fd)
+//                 printf("error\n"); // to do
+//         }
+//         else if (curr->redir_type == DREDIR_OUT)
+//         {
+//             fd = open(curr->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+//              if (!fd)
+//                 printf("error\n"); // to do
+//         }
+//         else if (curr->redir_type == REDIR_IN)
+//         {
+//             fd = open(curr->file, O_RDONLY);
+//             if (fd == -1)
+//             {
+//                return (-1); 
+//                 printf("the file does not exict\n"); // to do
+//             }
+//         }
+//         if (curr->next == NULL)
+//         {
+//             if (curr->redir_type == REDIR_OUT || curr->redir_type == DREDIR_OUT)
+//                 dup2(fd, 1);
+//             else
+//                 dup2(fd, 0);
+//             close(fd);
+//             return (0);
+//         }
+//         close(fd);
+//         curr = curr->next;
+//     }
+//     return (0);
+// }
+
+
+int check_the_redirection(t_commands *command)
 {
     t_redir *curr;
     int fd;
+
     curr = command->redir_lst;
     while (curr)
     {
         if (curr->redir_type == REDIR_OUT)
         {
-           fd = open(curr->file, O_WRONLY | O_CREAT  | O_TRUNC, 0644);
-           if (!fd)
-                printf("error\n"); // to do
+            fd = open(curr->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1)
+            {
+                printf("Error opening file for output redirection: %s\n", curr->file); // handle error
+                return (-1); 
+            }
         }
         else if (curr->redir_type == DREDIR_OUT)
         {
             fd = open(curr->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-             if (!fd)
-                printf("error\n"); // to do
+            if (fd == -1)
+            {
+                printf("Error opening file for append redirection: %s\n", curr->file); // handle error
+                return (-1);
+            }
         }
         else if (curr->redir_type == REDIR_IN)
         {
             fd = open(curr->file, O_RDONLY);
             if (fd == -1)
             {
-               return (-1); 
-                printf("the file does not exict\n"); // to do
+                printf("Error: file does not exist for input redirection: %s\n", curr->file); // handle error
+                return (-1);
             }
         }
+
+        // Perform the redirection on the last file descriptor
         if (curr->next == NULL)
         {
             if (curr->redir_type == REDIR_OUT || curr->redir_type == DREDIR_OUT)
-                dup2(fd, 1);
+                dup2(fd, STDOUT_FILENO);  // Redirect stdout
             else
-                dup2(fd, 0);
+                dup2(fd, STDIN_FILENO);   // Redirect stdin
             close(fd);
             return (0);
         }
-        close(fd);
+
+        close(fd); // Close the file descriptor after processing this redirection
         curr = curr->next;
     }
+
     return (0);
 }
