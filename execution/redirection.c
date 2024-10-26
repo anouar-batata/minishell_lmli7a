98a9b6b6391749b47fa5f54088aff7247c446a97
Anouar Louriga <alouriga@e2r8p7.1337.ma>
2024-10-26 05:56:09 +0100

    lol

--- execution/redirection.c
+++ execution/redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akoutate <akoutate@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alouriga <alouriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 23:42:01 by alouriga          #+#    #+#             */
-/*   Updated: 2024/10/25 01:57:39 by akoutate         ###   ########.fr       */
+/*   Updated: 2024/10/26 05:42:03 by alouriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,60 +56,122 @@
 // }
 
 
+// int check_the_redirection(t_commands *command)
+// {
+//     t_redir *curr;
+//     int fd;
+
+//     curr = command->redir_lst;
+//     while (curr)
+//     {
+//         if (curr->redir_type == REDIR_IN)
+//         {
+//             fd = open(curr->file, O_RDONLY);
+//             if (fd == -1)
+//             {
+//                 perror("Error: file does not exist for input redirection"); // handle error
+//                 return (-1);
+//             }
+// 			if (curr->to_close)
+// 				unlink(ft_strjoin2("./", curr->file));
+//         }
+//         if (curr->next == NULL)
+//         {
+//             dup2(fd, STDIN_FILENO);  
+//             close(fd);
+//             return (0);
+//         }
+//         curr = curr->next;
+//     }
+
+//     curr = command->redir_lst;
+//     while (curr)
+//     {
+//         if (curr->redir_type == REDIR_OUT)
+//         {
+//             fd = open(curr->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+//             if (fd == -1)
+//             {
+//                 perror("Error opening file for output redirection"); // handle error
+//                 return (-1); 
+//             }
+//         }
+//         else if (curr->redir_type == DREDIR_OUT)
+//         {
+//             fd = open(curr->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+//             if (fd == -1)
+//             {
+//                 perror("Error opening file for append redirection"); // handle error
+//                 return (-1);
+//             }
+//         }
+//         if (curr->next == NULL)
+//         {
+//             dup2(fd, STDOUT_FILENO);
+//             close(fd);
+//             return (0);
+//         }
+//         curr = curr->next;
+//     }    
+//     return (0);
+// }
+
+
 int check_the_redirection(t_commands *command)
 {
     t_redir *curr;
-    int fd;
+    int fd = -1;
 
+    // Handle input redirections
     curr = command->redir_lst;
     while (curr)
     {
-        if (curr->redir_type == REDIR_OUT)
+        if (curr->redir_type == REDIR_IN)
         {
-            fd = open(curr->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fd == -1)
-            {
-                perror("Error opening file for output redirection"); // handle error
-                return (-1); 
-            }
-        }
-        else if (curr->redir_type == DREDIR_OUT)
-        {
-            fd = open(curr->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-            if (fd == -1)
-            {
-                perror("Error opening file for append redirection"); // handle error
-                return (-1);
-            }
-        }
-        else if (curr->redir_type == REDIR_IN)
-        {
+            if (fd != -1) close(fd);  // Close previous fd if any
             fd = open(curr->file, O_RDONLY);
             if (fd == -1)
             {
-                perror("Error: file does not exist for input redirection"); // handle error
+                perror("Error: file does not exist for input redirection");
                 return (-1);
             }
-			if (curr->to_close)
-				unlink(ft_strjoin2("./", curr->file));
+            if (curr->to_close)
+            {
+                char *temp = ft_strjoin2("./", curr->file);
+                unlink(temp);
+                free(temp);
+            }
         }
-
-        // Perform the redirection on the last file descriptor
-        if (curr->next == NULL)
-        {
-            if (curr->redir_type == REDIR_OUT || curr->redir_type == DREDIR_OUT)
-                dup2(fd, STDOUT_FILENO);  // Redirect stdout
-            else
-                dup2(fd, STDIN_FILENO);   // Redirect stdin
-            close(fd);
-            return (0);
-        }
-
-        close(fd); // Close the file descriptor after processing this redirection
         curr = curr->next;
+    }
+    if (fd != -1)  // Apply input redirection if any
+    {
+        dup2(fd, STDIN_FILENO);
+        close(fd);
+    }
+
+    // Handle output redirections
+    curr = command->redir_lst;
+    fd = -1;
+    while (curr)
+    {
+        if (curr->redir_type == REDIR_OUT || curr->redir_type == DREDIR_OUT)
+        {
+            if (fd != -1) close(fd);  // Close previous fd if any
+            fd = open(curr->file, O_WRONLY | O_CREAT | (curr->redir_type == REDIR_OUT ? O_TRUNC : O_APPEND), 0644);
+            if (fd == -1)
+            {
+                perror("Error opening file for output redirection");
+                return (-1);
+            }
+        }
+        curr = curr->next;
+    }
+    if (fd != -1)  // Apply output redirection if any
+    {
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
     }
 
     return (0);
 }
-
-
---
+++ lol
@@ -0,0 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: akoutate <akoutate@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/07/31 23:21:04 by alouriga          #+#    #+#              #
+#    Updated: 2024/10/11 14:47:18 by akoutate         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = minishell
+
+SOURCES = built_ins/cd.c built_ins/echo.c built_ins/env_utils.c \
+			built_ins/env.c built_ins/exit.c built_ins/export.c \
+			execution/main.c built_ins/pwd.c built_ins/split_2.c \
+			built_ins/split.c built_ins/unset.c built_ins/utils.c \
+			execution/utils.c built_ins/mini_main.c execution/redirection.c \
+			execution/execute_pipes.c execution/exit_status_utils.c \
+			error_handling.c expanding.c ft_lstadd_back.c ft_lstnew.c \
+			ft_lstsize.c ft_split.c ft_strjoin.c join.c louriga_aviable.c \
+			minishell.c split_word.c utils.c words_join.c clean_list.c \
+			heredo9.c
+
+OBJ = $(SOURCES:.c=.o)
+CC = cc
+# CFLAGS = -Wall -Wextra -Werror -g
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(OBJ) -o $@ -g -lreadline 
+
+%.o: %.c minishell.h
+	$(CC) $(CFLAGS) -c $< -g -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
